14927377398838bad613c8bf5825db37661fb2d7
sunggeun yoo <orcogre@gmail.com>
2023-04-19 17:18:45 +0900

    add default timestamp

--- sps30Grabber.py
+++ sps30Grabber.py
@@ -14,9 +14,9 @@ data = sps30.read_values()
 
 conn = sqlite3.connect('sps30.db')
 cur = conn.cursor()
-conn.execute('CREATE TABLE IF NOT EXISTS sps30(id INTEGER PRIMARY KEY AUTOINCREMENT, pm1_0 REAL, pm2_5 REAL, pm4_0 REAL, pm10_0 REAL, nc0_5 REAL, nc1_0 REAL, nc2_5 REAL, nc4_5 REAL, nc10_0 REAL, typical_particle_size REAL, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)')
+conn.execute("CREATE TABLE IF NOT EXISTS sps30(id INTEGER PRIMARY KEY AUTOINCREMENT, pm1_0 REAL, pm2_5 REAL, pm4_0 REAL, pm10_0 REAL, nc0_5 REAL, nc1_0 REAL, nc2_5 REAL, nc4_5 REAL, nc10_0 REAL, typical_particle_size REAL, timestamp DATETIME DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')))")
 conn.commit()
-conn.execute('INSERT INTO sps30 VALUES(NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, null)', data)
+conn.execute("INSERT INTO sps30 (pm1_0, pm2_5, pm4_0, pm10_0, nc0_5, nc1_0, nc2_5, nc4_5, nc10_0, typical_particle_size) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data)
 conn.commit()
 conn.close()
     
